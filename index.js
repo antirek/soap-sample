3c323b405efa82eb3282ee77f65196c0c705634d
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-02-09 16:36:11 +0700

    fix

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
   var soap = require('soap');
-  var url = 'https://api4mini.webmoney.ru/AppActivationApi.asmx?wsdl';
+//  var url = 'https://api4mini.webmoney.ru/AppActivationApi.asmx?wsdl';
+  var url = 'http://127.0.0.1:8000/wsdl?wsdl';
   var args = {appId: 'XXXX'};
+
   soap.createClient(url, function (err, client) {
-      client.NewSessionForServerApp(args, function (err, result) {          
+     console.dir(client);
+
+/*      client.NewSessionForServerApp(args, function (err, result) {          
           console.log('sessionId:', result.sessionId);
       });
+*/
   });
---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var soap = require('soap');
+var helloService = {
+  Hello_Service: {
+    Hello_Port: {
+      SayHelloRequest: function(args) {
+        return {
+          firstName: args.name
+        };
+      }
+    }
+  }
+}
+var xml = require('fs').readFileSync('./scheme.wsdl', 'utf8'),
+      server = http.createServer(function(request,response) {
+          response.end("404: Not Found: "+request.url)
+      });
+server.listen(8000);
+soap.listen(server, '/wsdl', helloService, xml);
